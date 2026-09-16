65ffd5fea09f24f5a5f078b9076a5362de912df7
Ayas Ibrahim <aayasabraham@gmail.com>
2026-09-16 18:25:28 +0600

    feat: refactor program routes and update import paths for middleware and validation

--- src/app/module/programe/programe.route.ts
+++ src/app/module/programe/programe.route.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { ProgramController } from "./program.controller";
-import auth from "../../middlewares/auth";
-import validateRequest from "../../middlewares/validateRequest";
-import { ProgramValidation } from "./program.validation";
+import { auth } from "../../middleware/checkAuth";
+import { validateRequest } from "../../middleware/validateReques";
+import { ProgramValidation } from "./programe.validation";
+
 
 const router = Router();
 
@@ -11,20 +11,20 @@ router.post(
   "/create-program",
   auth("SUPER_ADMIN", "REGISTRAR"), // শুধুমাত্র সুপার অ্যাডমিন ও রেজিস্ট্রার ডিগ্রি প্ল্যান তৈরি করতে পারবে
   validateRequest(ProgramValidation.CreateProgramZodSchema),
-  ProgramController.createProgram
+//   ProgramController.createProgram
 );
 
 // ২. সকল প্রোগ্রামের লিস্ট দেখা (Pagination, Search & Filtering সহ)
 // এটি সবার জন্য পাবলিক রাখা হয়েছে ড্রপডাউন এবং কারিকুলাম ব্রাউজ করার সুবিধার জন্য
 router.get(
   "/",
-  ProgramController.getAllPrograms
+//   ProgramController.getAllPrograms
 );
 
 // ৩. নির্দিষ্ট একটি প্রোগ্রামের ডিটেইলস দেখা (ID দিয়ে)
 router.get(
   "/:id",
-  ProgramController.getProgramById
+//   ProgramController.getProgramById
 );
 
 // ৪. প্রোগ্রামের তথ্য আপডেট করা (Update)
@@ -32,14 +32,14 @@ router.patch(
   "/:id",
   auth("SUPER_ADMIN", "REGISTRAR", "DEPARTMENT_ADMIN"), // ডিপার্টমেন্ট হেডও তার নিজস্ব প্রোগ্রাম মডিফাই করতে পারবে
   validateRequest(ProgramValidation.UpdateProgramZodSchema),
-  ProgramController.updateProgram
+//   ProgramController.updateProgram
 );
 
 // ۵. প্রোগ্রাম সফট ডিলিট করা (Soft Delete)
 router.delete(
   "/:id",
   auth("SUPER_ADMIN"), // ডিগ্রি বা প্রোগ্রাম বাতিল করার মতো সেন্সিটিভ ডিসিশন শুধু সুপার অ্যাডমিন নিতে পারবে
-  ProgramController.deleteProgram
+//   ProgramController.deleteProgram
 );
 
 export const ProgramRoutes = router;
