09eca40d700e66d6158e18da83066670836dc811
Ayas Ibrahim <aayasabraham@gmail.com>
2026-09-20 23:28:26 +0600

    feat: add node-cron dependency and implement cron job for deleting unverified student applications

--- src/app/lib/cron.ts
+++ src/app/lib/cron.ts
@@ -1,35 +1,33 @@
-import cron from 'node-cron';
-import { DoctorVerificationStatus, Role } from '../../generated/prisma/enums';
-import { prisma } from './prisma';
+import cron from "node-cron";
+import { prisma } from "./prisma";
 
+export const deleteUnverifiedStudentApplications = async () => {
+  cron.schedule("*/10 * * * *", async () => {
+    try {
+      const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000);
 
-export const deleteUnverifiedDoctors = async () => {
-    cron.schedule('*/10 * * * *', async () => {
+      const deletedApplications = await prisma.studentApplication.deleteMany({
+        where: {
+          emailVerified: false,
+          status: "PENDING",
+          createdAt: {
+            lt: oneHourAgo,
+          },
+        },
+      });
 
-       try {
-           const oneHourAgo = new Date(Date.now() - 60 * 60 * 1000)
-           const deletedDoctors = await prisma.user.deleteMany({
-               where: {
-                   role: Role.DOCTOR,
-                   emailVerified: false,
-                   createdAt: { lt: oneHourAgo },
-                   doctor: {
-                       verificationStatus: DoctorVerificationStatus.PENDING
-                   }
-               }
-           });
+      if (deletedApplications.count > 0) {
+        console.log(
+          `Cron: Deleted ${deletedApplications.count} unverified student applications older than 1 hour.`,
+        );
+      }
+    } catch (error) {
+      console.log(
+        "Cron: Failed to delete unverified student applications.",
+        error,
+      );
+    }
 
-
-           if (deletedDoctors.count > 0) {
-               console.log(`
-                Cron: Deleted ${deletedDoctors.count} unverified email doctor applications older than 1 hour
-                `);
-           }
-       } catch (error) {
-
-            console.log("Cron: Failed to delete unverified doctor applications", error);
-       }
-
-       console.log("Unverified Doctor Delete cron schedule (every 10 minutes)");
-    });
-}
+    console.log("Unverified student application cleanup cron is running.");
+  });
+};
